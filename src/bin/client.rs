d930affc9f67cb8cb490908901b4cc6a27a2ffaf
Rohit <rohitnarnolia88@gmail.com>
2026-05-01 17:17:20 +0530

    append request with a len header

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,15 +1,63 @@
-use std::io::{ Read, Write };
+use std::io::{ ErrorKind, Read, Write };
 use std::net::TcpStream;
+const MAX_MSG: usize = 4096;
+
+fn query(stream: &mut TcpStream, text: &str) -> Result<(), ()> {
+    let text_bytes = text.as_bytes();
+    if text_bytes.len() > MAX_MSG {
+        eprintln!("message is too long");
+        return Err(());
+    }
+
+    // building the request
+
+    let len = text_bytes.len() as u32; // header bnega
+    let mut send_buf = Vec::new();
+    send_buf.extend_from_slice(&len.to_le_bytes()); // header .. length
+    send_buf.extend_from_slice(text_bytes); // actual message
+
+    // send this
+
+    stream.write_all(&send_buf).map_err(|_| {
+        eprintln!("write() error");
+    })?;
+
+    // now we read the reply
+    // first 4 bytes
+    let mut header = [0u8; 4];
+    stream.read_exact(&mut header).map_err(|e| {
+        if e.kind() == ErrorKind::UnexpectedEof {
+            eprintln!("EOF");
+        } else {
+            eprintln!("read() error: {}", e);
+        }
+    })?;
+
+    // get length of the reply
+    let reply_len = u32::from_le_bytes(header) as usize;
+
+    if reply_len > MAX_MSG {
+        eprintln!("reply too long");
+        return Err(());
+    }
+
+    // get the actual res from server
+    let mut body = vec![0u8;reply_len];
+
+    stream.read_exact(&mut body).map_err(|_| {
+        eprintln!("read() error");
+    })?;
+
+    // print out the message from server
+
+    println!("server says : {}", String::from_utf8_lossy(&body));
+    Ok(())
+}
 fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:1234").expect("connect() failed");
 
-    stream.write_all(b"hello").expect("write() failed");
-
-    let mut buf = [0u8; 64];
-    let n = stream.read(&mut buf).expect("read() failed");
-
-    let msg = std::str::from_utf8(&buf[..n]).unwrap_or("(invalid utf8)");
-    println!("server says: {}", msg);
-
+    query(&mut stream, "hello1").ok();
+    query(&mut stream, "hello2").ok();
+    query(&mut stream, "hello3").ok();
     // close() - automatic as the stream drops
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,30 +1,78 @@
-use std::{ io::{ Read, Write }, net::{ TcpListener, TcpStream } };
+use std::{ io::{ ErrorKind, Read, Write }, net::{ TcpListener, TcpStream } };
 
-fn do_something(mut stream: TcpStream) {
-    let mut buf = [0u8; 64];
-    let n = stream.read(&mut buf).unwrap_or_else(|e| {
-        eprintln!("read() error: {}", e);
-        0
-    });
+const MAX_MSG: usize = 4096;
+fn one_request(stream: &mut TcpStream) -> Result<(), ()> {
+    // 1. read exac 4 bytes
+    let mut header = [0u8; 4];
+    stream.read_exact(&mut header).map_err(|e| {
+        if e.kind() == ErrorKind::UnexpectedEof {
+            eprintln!("EOF");
+        } else {
+            eprintln!("read() error: {}", e);
+        }
+    })?;
 
-    if n == 0 {
-        return;
+    // 2. now we can parse the remaining
+    let len = u32::from_le_bytes(header) as usize; // parsing the length here ..  ex 6 0 0 0 becomes 6
+
+    if len > MAX_MSG {
+        eprintln!("too long");
+        return Err(());
     }
 
-    let msg = str::from_utf8(&buf[..n]).unwrap_or("invalid utf8");
+    //3. read exac len bytes
+    let mut body = vec![0u8; len];
+    stream.read_exact(&mut body).map_err(|_| {
+        eprintln!("read() error");
+    })?;
+
+    // 4. print msg
+    let msg = String::from_utf8_lossy(&body);
     println!("client says: {}", msg);
 
-    stream.write_all(b"ha bhai kya hua").unwrap()
+    let reply = b"world";
+    let reply_len = reply.len() as u32;
+
+    //5. send a reply
+    // we will also attach the length in begining
+    let mut reply_buf = Vec::new();
+    reply_buf.extend_from_slice(&reply_len.to_le_bytes()); // header
+    reply_buf.extend_from_slice(reply);
+
+    stream.write_all(&reply_buf).map_err(|_| { eprintln!("write() error") })?;
+    Ok(())
 }
+
+// fn do_something(mut stream: TcpStream) {
+//     let mut buf = [0u8; 64];
+//     let n = stream.read(&mut buf).unwrap_or_else(|e| {
+//         eprintln!("read() error: {}", e);
+//         0
+//     });
+
+//     if n == 0 {
+//         return;
+//     }
+
+//     let msg = str::from_utf8(&buf[..n]).unwrap_or("invalid utf8");
+//     println!("client says: {}", msg);
+
+//     stream.write_all(b"ha bhai kya hua").unwrap()
+// }
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:1234").expect("failed to bind");
 
     println!("Listening on the port 1234");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(conn) => {
-                do_something(conn);
+    for res in listener.incoming() {
+        match res {
+            Ok(mut conn) => {
+                loop {
+                    if one_request(&mut conn).is_err() {
+                        break;
+                    }
+                }
+                // close() automatic when dropped
             }
             Err(e) => {
                 eprintln!("accept() err: {}", e);
